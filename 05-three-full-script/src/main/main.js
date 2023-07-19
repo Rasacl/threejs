8274bb8bc15f4ee1d04fb394a98433db81e9868e
chenxiao <chenxiao@uino.com>
2023-07-19 11:16:50 +0800

    3D屏幕

--- 05-three-full-script/src/main/main.js
+++ 05-three-full-script/src/main/main.js
@@ -47,7 +47,87 @@ for (let i = 0; i < 5; i++) {
     }
   }
 }
+
+
+//c创建三角形
+var sjxGroup = new THREE.Group();
+for (let i = 0; i < 50; i++) {
+  // 每一个三角形，需要3个顶点，每个顶点需要3个值
+  const geometry = new THREE.BufferGeometry();
+  const positionArray = new Float32Array(9);
+  for (let j = 0; j < 9; j++) {
+    if(j%3 == 1){
+      positionArray[j] = Math.random() * 10 - 5;
+    }else {
+
+      positionArray[j] = Math.random() * 10 - 5;
+    }
+  }
+  geometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(positionArray, 3)
+  );
+  let color = new THREE.Color(Math.random(), Math.random(), Math.random());
+  const material = new THREE.MeshBasicMaterial({
+    color: color,
+    transparent: true,
+    opacity: 0.5,
+    side: THREE.DoubleSide,
+  });
+  // 根据几何体和材质创建物体
+  let sjxmesh = new THREE.Mesh(geometry, material);
+  sjxGroup.add(sjxmesh);
+  sjxGroup.position.set(0, -35, 0);
+}
+
+//弹跳小球
+let sphereGroup = new THREE.Group();
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial();
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+// 投射阴影
+sphere.castShadow = true;
+
+sphereGroup.add(sphere);
+
+// // 创建平面
+const planeGeometry = new THREE.PlaneGeometry(50, 50);
+const plane = new THREE.Mesh(planeGeometry, sphereMaterial);
+plane.position.set(0, -1, 0);
+plane.rotation.x = -Math.PI / 2;
+// 接收阴影
+plane.receiveShadow = true;
+sphereGroup.add(plane);
+
+// 灯光
+// 环境光
+const light = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
+sphereGroup.add(light);
+
+const smallBall = new THREE.Mesh(
+    new THREE.SphereGeometry(0.1, 20, 20),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+smallBall.position.set(2, 2, 2);
+//直线光源
+const pointLight = new THREE.PointLight(0xff0000, 2);
+// pointLight.position.set(2, 2, 2);
+pointLight.castShadow = true;
+
+// 设置阴影贴图模糊度
+pointLight.shadow.radius = 20;
+// 设置阴影贴图的分辨率
+pointLight.shadow.mapSize.set(512, 512);
+pointLight.decay = 0;
+sphereGroup.position.set(0, -75, 0);
+// 设置透视相机的属性
+smallBall.add(pointLight);
+sphereGroup.add(smallBall);
+scene.add(sphereGroup);
 scene.add(cubeGroup)
+scene.add(sjxGroup)
+
+let arrGroup = [cubeGroup,sjxGroup,sphereGroup];
 // 创建投射光线对象
 const raycaster = new THREE.Raycaster();
 
@@ -55,18 +135,11 @@ const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
 // 监听鼠标的位置
-// window.addEventListener("mousemove", (event) => {
-//   //   console.log(event);
-//   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   mouse.y = -((event.clientY / window.innerHeight) * 2 - 1);
-//   raycaster.setFromCamera(mouse, camera);
-//   let result = raycaster.intersectObjects(cubeArr);
-//   //   console.log(result);
-//   //   result[0].object.material = redMaterial;
-//   result.forEach((item) => {
-//     item.object.material = redMaterial;
-//   });
-// });
+window.addEventListener("mousemove", (event) => {
+  //摇晃相机
+  mouse.x = (event.clientX / window.innerWidth)  - 0.5;
+  mouse.y = (event.clientY / window.innerHeight) - 0.5;
+});
 
 // 监听鼠标的位置
 window.addEventListener("click", (event) => {
@@ -81,6 +154,24 @@ window.addEventListener("click", (event) => {
     item.object.material = redMaterial;
   });
 });
+//设置当前页
+let currentPage = 0;
+//监听鼠标滚动事件
+window.addEventListener("scroll", (event) => {
+  const newPage = Math.round(window.scrollY / window.innerHeight);
+if (currentPage !== newPage){
+  currentPage = newPage;
+  console.log('改变页面', currentPage)
+// gsap.to(`.page${currentPage} h1`, {
+//     duration: 0.7,
+//   rotation: '+=360',
+// })
+  gsap.fromTo(`.page${currentPage} h1`,
+      {x:-300},
+      {x:0,duration: 0.7,rotation: '+=360',}
+  )
+}
+})
 
 // 初始化渲染器
 const renderer = new THREE.WebGLRenderer({alpha: true});
@@ -109,9 +200,22 @@ const clock = new THREE.Clock();
 
 function render() {
   let time = clock.getElapsedTime();
+  let delTime = clock.getDelta();
   cubeGroup.rotation.x = time * 0.5;
   cubeGroup.rotation.y = time * 0.5;
+
+  sjxGroup.rotation.x = time * 0.5;
+  sjxGroup.rotation.y = time * 0.5;
+
+  smallBall.position.x = Math.sin(time) * 3;
+  smallBall.position.z = Math.cos(time) * 3;
+  smallBall.position.y = 2 + Math.sin(time * 10) / 2;
+  sphereGroup.rotation.z = Math.sin(time)  * 0.5;
+  sphereGroup.rotation.y = Math.sin(time)  * 0.1;
   // controls.update();
+  //根据当前滚动的scrolly值，设置摄像机的位置
+  camera.position.y = -(window.scrollY / window.innerHeight) * 30;
+  camera.position.x = (mouse.x * 10 - camera.position.x) * delTime * 50;
   renderer.render(scene, camera);
   //   渲染下一帧的时候就会调用render函数
   requestAnimationFrame(render);
